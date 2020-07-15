2dfadf91bfacc0c26745fbbf502201f45644137c
Raveesha Fernando <rdilshanf@gmail.com>
2020-07-15 23:35:28 +0530

    home page added with route

--- frontend/src/main/java/com/quiz/frontend/controler/maincontroller.java
+++ frontend/src/main/java/com/quiz/frontend/controler/maincontroller.java
@@ -10,11 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class maincontroller {
     @RequestMapping(value = "/index")
-   public String index(Model model){
-       String name="pradeepa Sandaruwan";
-    //    data ("pradeepa");
-    //    data.add("Sandaruwan");
-        model.addAttribute("name",name);  
+    public String index(final Model model) {
+        final String name = "pradeepa Sandaruwan";
+        // data ("pradeepa");
+        // data.add("Sandaruwan");
+        model.addAttribute("name", name);
         return "index";
-   }
+    }
+
+    @RequestMapping(value = "/home")
+    public String home(final Model model) {
+        return "home";
+    }
 }
