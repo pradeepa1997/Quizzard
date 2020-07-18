bc27d3fa876a9e5cd4f72113e415c708740471cd
Raveesha Fernando <rdilshanf@gmail.com>
2020-07-18 22:42:43 +0530

    quiz page

--- frontend/src/main/java/com/quiz/frontend/controler/maincontroller.java
+++ frontend/src/main/java/com/quiz/frontend/controler/maincontroller.java
@@ -1,8 +1,5 @@
 package com.quiz.frontend.controler;
 
-import java.lang.reflect.Array;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class maincontroller {
     @RequestMapping(value = "/index")
     public String index(final Model model) {
-        final String name = "pradeepa Sandaruwan";
-        // data ("pradeepa");
-        // data.add("Sandaruwan");
-        model.addAttribute("name", name);
         return "index";
     }
 
     @RequestMapping(value = "/home")
     public String home(final Model model) {
         return "home";
+    }
+
+    @RequestMapping(value = "/quiz")
+    public String quiz(final Model model) {
+        return "quiz";
     }
 }
